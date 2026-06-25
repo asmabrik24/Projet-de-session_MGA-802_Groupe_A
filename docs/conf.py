f4a52b0333312566fdb714f110a43254260ffb1b
asmabrik24 <asma.brik.1@ens.etsmtl.ca>
2026-06-25 00:02:44 -0400

    Documentation sphinx (#21)
    * Documentation Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Fusion GPS/IMU pour la navigation véhiculaire à l’aide de Python'
+copyright = '2026, Asma brik, Nada CHAOUACHI, Brahim REZGUI'
+author = 'Asma brik, Nada CHAOUACHI, Brahim REZGUI'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
